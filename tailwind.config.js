920bdd394d32803bffa61761052d2030b62868e8
emilsultanov <emilsultanov01@gmail.com>
2023-07-18 23:29:44 +0400

    Make image card

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,12 @@ module.exports = {
         'primary-blue-200': '#2D60E4',
         'primary-gray-100': '#E3E3E3',
         'primary-gray-200': '#D0D0D0',
+      },
+      backgroundImage:{
+        'plastic-surgery': "url('/background-images/plastic-surgery.svg')"
+      },
+      fontSize:{
+        "2xs": "0.8rem"
       }
     }
   },
